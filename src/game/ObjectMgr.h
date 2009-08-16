96691fbdf0398cacd39726d9893e25ac15a3817d
Fabi <fabian@wow-extrem.de>
2009-08-16 15:56:52 +0200

    [56] Deleted CheckDB option from Core.

--- src/game/ObjectMgr.h
+++ src/game/ObjectMgr.h
@@ -869,7 +869,6 @@ class ObjectMgr
         GmTicketList m_GMTicketList;
         uint64 GenerateGMTicketId();
 
-        bool CheckDB() const;
     protected:
 
         // first free id for selected id type
