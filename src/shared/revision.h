22b1ed3e44a2052557363f042d5f5b046a54c899
Fabi <fabian@wow-extrem.de>
2009-09-01 21:56:24 +0200

    [103] Updated full sql's.

--- src/shared/revision.h
+++ src/shared/revision.h
@@ -1,7 +1,7 @@
 #ifndef __REVISION_H__
 #define __REVISION_H__
- #define _REVISION "102"
+ #define _REVISION "103"
  #define _HASH ""
  #define _REVISION_DATE "01.09.2009"
-#define _REVISION_TIME "21:48"
+#define _REVISION_TIME "21:55"
 #endif // __REVISION_H__
