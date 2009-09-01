c9031a506cb63b4be1153fc74787a31a4080f60d
Fabian <fabian@wow-extrem.de>
2009-09-01 20:25:25 +0200

    [101] Add some more debug info, don't kick GMs :)

--- src/shared/revision.h
+++ src/shared/revision.h
@@ -1,7 +1,7 @@
 #ifndef __REVISION_H__
 #define __REVISION_H__
- #define _REVISION "100"
+ #define _REVISION "101"
  #define _HASH ""
  #define _REVISION_DATE "01.09.2009"
-#define _REVISION_TIME "20:22"
+#define _REVISION_TIME "20:25"
 #endif // __REVISION_H__
