d49e6b31a0583dd89168282f6e9e4fedcad12191
Fabian <fabian@wow-extrem.de>
2009-09-02 16:06:10 +0200

    [105] Try to fix loading character list.

--- src/shared/revision.h
+++ src/shared/revision.h
@@ -1,7 +1,7 @@
 #ifndef __REVISION_H__
 #define __REVISION_H__
- #define _REVISION "104"
+ #define _REVISION "105"
  #define _HASH ""
  #define _REVISION_DATE "02.09.2009"
-#define _REVISION_TIME "14:10"
+#define _REVISION_TIME "16:06"
 #endif // __REVISION_H__
